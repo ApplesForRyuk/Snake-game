9ed286fd021ec4633cf0f0e6de3d7b965b8b64c2
Μυρτώ Βασιλείου <murtov21@gmail.com>
2022-12-13 00:48:10 +0200

    building the fountation of the app

---
+++ #python snake game.py
@@ -0,0 +1,16 @@
+#python snake game
+import math
+import random
+import pygame
+import _tkinter as tk
+from tkinter import messagebox
+
+class cube(object): 
+    rows = 0
+    w = 0
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+
+    def move(self,dirnx,dirny):
+        pass
+    
