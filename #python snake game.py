4bd460b3a993be89d97da95a886e7c8ab7519d2c
Μυρτώ Βασιλείου <murtov21@gmail.com>
2022-12-14 01:13:02 +0200

    feat: setting the turning positions of snake

--- #python snake game.py
+++ #python snake game.py
@@ -31,7 +31,42 @@ class snake(object):
         pass
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+            
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.head[self.head.pos[:]] = [self.dirnx,self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.head[self.head.pos[:]] = [self.dirnx,self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.head[self.head.pos[:]] = [self.dirnx,self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.head[self.head.pos[:]] = [self.dirnx,self.dirny]
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p] 
+                c.move(turn[0],turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+
+                
+        
 
     def reset(pos):
         pass
